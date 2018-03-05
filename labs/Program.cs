d0fb592e04d5b74e312adfa398416c1c6f7dc0f7
dmitryblackwell <dmitryblackwell@gmail.com>
2018-03-05 23:57:00 +0200

    Project init
    Create Project

---
+++ labs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 27
+namespace labs
+{
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("Dasha, I Love you");
+            Console.ReadKey();
+        }
+    }
+}
