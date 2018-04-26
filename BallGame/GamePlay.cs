75b2709de79f05e14d4ef7618e11a35ae9eb9042
dmitryblackwell <dmitryblackwell@gmail.com>
2018-04-27 00:32:09 +0300

    New Ball. GamePlay.
    Create multithreading for input and fix some bugs. Added posibility of cloning shilds and swap tham.

--- BallGame/GamePlay.cs
+++ BallGame/GamePlay.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace OOP_Concepts.BallGame
 {
     class GamePlay
     {
+        // карта в виде массива строк
         private String[] MapStr =
         {
             "##################",
@@ -16,45 +17,68 @@ namespace OOP_Concepts.BallGame
             "#       @        #",
             "##################"
         };
-        Map map;
+        private const int UPDATE_TIME = 400; // время задержки между обновлениями
+        private Map map; // наша карта
+        //static void Main(string[] args) { new GamePlay(); } // создание этого класса
         public GamePlay()
         {
-            map = new Map(MapStr);
+            map = new Map(MapStr); // инициализация карты
             Run();
         }
         private void Run()
         {
             bool isExit = false;
+            // запуск нового потока, для получения нажатых клавишь
+            Thread input = new Thread(() =>
+            {
+                while (map.isGameOn() && !isExit)
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    switch (cki.Key)
+                    {
+                        case ConsoleKey.A:
+                        case ConsoleKey.LeftArrow:
+                            map.moveLeft();
+                            break;
+                        case ConsoleKey.D:
+                        case ConsoleKey.RightArrow:
+                            map.moveRight();
+                            break;
+                        case ConsoleKey.W:
+                        case ConsoleKey.UpArrow:
+                            map.moveUp();
+                            break;
+                        case ConsoleKey.S:
+                        case ConsoleKey.DownArrow:
+                            map.moveDown();
+                            break;
+                        case ConsoleKey.Q:
+                        case ConsoleKey.E:
+                            isExit = true;
+                            break;
+                        case ConsoleKey.F:
+                            map.swapShields();
+                            break;
+                        case ConsoleKey.Spacebar:
+                            map.settleShield();
+                            break;
+                    }
+                }
+            });
+            input.Start();// его запуск
             while (map.isGameOn() && !isExit)
             {
                 Console.Clear(); // все стираем
                 map.draw();// Выводим нашу карту
-                // Ждем пол секунды пока пользователь подвинет щит
-                long StartTime = DateTime.Now.Millisecond;
-
-                ConsoleKeyInfo cki = new ConsoleKeyInfo();
-                Task.Factory.StartNew(() => cki = Console.ReadKey())
-                .Wait(TimeSpan.FromSeconds(1));
-                switch (cki.Key)
-                {
-                    case ConsoleKey.LeftArrow:
-                        map.moveLeft();
-                        break;
-                    case ConsoleKey.RightArrow:
-                        map.moveRight();
-                        break;
-                    case ConsoleKey.UpArrow:
-                        map.moveUp();
-                        break;
-                    case ConsoleKey.DownArrow:
-                        map.moveDown();
-                        break;
-                    case ConsoleKey.E:
-                        isExit = true;
-                        break;
-                }
 
                 map.BallStep(); // двигаем мячик
+                Thread.Sleep(UPDATE_TIME); // ждем
+            }
+            if (!map.isGameOn()) // если победили
+            {
+                Console.WriteLine("\nYou win!\n Press any key to continue.");
+                Console.ReadKey();
             }
         }
     }
--- BallGame/Map.cs
+++ BallGame/Map.cs
@@ -1,34 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
 
 namespace OOP_Concepts.BallGame
 {
+    // структура с кординатами x y
+    struct Point
+    {
+        public int X;
+        public int Y;
+        public Point(int x_, int y_) { X = x_; Y = y_; }
+    }
     class Map
     {
-        private char[,] map;
-        private Point shield;
-        private Point ball;
-        private int vX = -1;
-        private int vY = 0;
+        private char[,] map; // двухмерный массив чаров
+        private Point shield; // кординаты щита
+        private Point ball; // кординаты мяча
+        private int vX = -1; // направление движения по Х, может быть -1, 0, 1
+        private int vY = 0; // тоже самое для y
+        private int totalBalls = 0; // всего мячей
+        private int ballsDown = 0; // мячей сбили
+        private bool settlementShield = false; // нужно ли устанавливать щит
+
         public Map(String[] MapStr)
         {
-            map = new char[MapStr.Length,MapStr[0].Length];
+            // инициализируем карту
+            map = new char[MapStr.Length, MapStr[0].Length];
             for (int i = 0; i < map.GetLength(0); ++i)
                 for (int j = 0; j < map.GetLength(1); ++j)
                 {
-                    map[i, j] = MapStr[i][j];
-                    if (map[i, j] == '/' || map[i,j] == '\\')
+                    map[i, j] = MapStr[i][j]; // копируем туда символ из строки
+                    if (map[i, j] == '/' || map[i, j] == '\\')
                         shield = new Point(j, i);
                     if (map[i, j] == '.')
                         ball = new Point(j, i);
+                    if (map[i, j] == '@')
+                        totalBalls++;
                 }
         }
 
 
-        public void draw()
+        public void draw() // рисум карту
         {
             for (int i = 0; i < map.GetLength(0); ++i)
             {
@@ -38,33 +48,41 @@ namespace OOP_Concepts.BallGame
             }
         }
 
-        public bool isGameOn()
+        public bool isGameOn() // проверка собрали мы мячи или нет
         {
-            return true; // TODO game ending
+            return ballsDown < totalBalls;
         }
 
+        // движение щита в разные стороны
         public void moveLeft() { move(-1, 0); }
         public void moveRight() { move(1, 0); }
         public void moveUp() { move(0, -1); }
         public void moveDown() { move(0, 1); }
 
+        // движение щита, вызывается методами выше
         private void move(int vX, int vY)
         {
             if (map[shield.Y + vY, shield.X + vX] == ' ')
             {
                 map[shield.Y + vY, shield.X + vX] = map[shield.Y, shield.X];
-                map[shield.Y, shield.X] = ' ';
+                if (!settlementShield) // если щит не надо устанавливать
+                    map[shield.Y, shield.X] = ' ';
                 shield.X += vX;
                 shield.Y += vY;
+                settlementShield = false;
             }
 
         }
 
+        // движение мячика
         public void BallStep()
         {
-            switch(map[ball.Y + vY, ball.X + vX])
+            switch (map[ball.Y + vY, ball.X + vX])
             {
                 case '@':
+                    ballsDown++;
+                    moveBall();
+                    break;
                 case ' ':
                     moveBall();
                     break;
@@ -74,7 +92,7 @@ namespace OOP_Concepts.BallGame
                     break;
                 case '\\':
                     swapVelocity();
-                    moveBall();
+                    changeBallCoords();
                     // -1  0 ==>  0 -1
                     //  1  0 ==>  0  1
                     //  0  1 ==>  1  0
@@ -84,7 +102,8 @@ namespace OOP_Concepts.BallGame
                     swapVelocity();
                     vX *= -1;
                     vY *= -1;
-                    moveBall();
+                    changeBallCoords();
+
                     //  1  0 ==>  0 -1
                     // -1  0 ==>  0  1
                     //  0  1 ==> -1  0
@@ -93,24 +112,38 @@ namespace OOP_Concepts.BallGame
             }
         }
 
+        // меняем переменны направления мяча
         private void swapVelocity()
         {
+            map[ball.Y, ball.X] = ' ';
+            changeBallCoords();
             int tmp = vX;
             vX = vY;
             vY = tmp;
         }
+        // передвигаем мячик
         private void moveBall()
         {
             map[ball.Y + vY, ball.X + vX] = '.';
             map[ball.Y, ball.X] = ' ';
             changeBallCoords();
         }
+        // двигаем мячик
         private void changeBallCoords()
         {
             ball.X += vX;
             ball.Y += vY;
         }
-
+        // меняем щиты
+        public void swapShields()
+        {
+            map[shield.Y, shield.X] = (map[shield.Y, shield.X] == '/') ? '\\' : '/';
+        }
+        // устанавливаем щит
+        public void settleShield()
+        {
+            settlementShield = true;
+        }
 
     }
 }
@@ -119,3 +152,5 @@ namespace OOP_Concepts.BallGame
 
 // The Veer Union - Another World Away
 // Blackbriar - Preserved Roses
+// Art of Chaos - Obsession (Surrender Me)
+// Into the Fire - Spit You Out
